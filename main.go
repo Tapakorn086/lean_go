6de481b5cce41a0861a042ae3f899d380392acec
Tapakorn086 <nakason_t@silpakorn.edu>
2025-06-28 10:21:20 +0700

    oop in go

--- main.go
+++ main.go
@@ -12,10 +12,23 @@ func (p *Point) move(x, y int) {
 	p.y += y
 }
 
+func (p *Point) display() string{
+	return fmt.Sprintf("Point: (%d, %d)", p.x, p.y)
+}
+
+type Car struct{
+	Point
+	Color string
+}
+
 func main() {
 	p := Point{x: 1, y: 2}
 	fmt.Println(p)
 	p.move(4, 5)
 	fmt.Println(p)
+
+	car := Car{Color:"red",Point: Point{x:5,y:10}}
+	car.move(2,3)
+	fmt.Println(car.display())
 
 }
