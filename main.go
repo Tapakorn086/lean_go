331ed71a33dad8d3d76fb17e4c1b9eb38ff8cfb8
Tapakorn086 <nakason_t@silpakorn.edu>
2025-06-28 10:25:40 +0700

    real struct

--- main.go
+++ main.go
@@ -1,43 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
 
-type Point struct {
-	x int
-	y int
-}
-
-func (p *Point) move(x, y int) {
-	p.x += x
-	p.y += y
-}
-
-func (p *Point) display() string {
-	return fmt.Sprintf("Point: (%d, %d)", p.x, p.y)
-}
-
-type Car struct {
-	Point
-	Color string
-}
-
-type Displayable interface {
-	display() string
+type User struct {
+	ID    int    `json"id"`
+	Name  string `json"name"`
+	Age   int    `json"age"`
+	Email string `json "email"`
 }
 
 func main() {
-	p := Point{x: 1, y: 2}
-	fmt.Println(p)
-	p.move(4, 5)
-	fmt.Println(p)
-
-	car := Car{Color: "red", Point: Point{x: 5, y: 10}}
-	car.move(2, 3)
-	fmt.Println(car.display())
-
-	var d Displayable = &car
-	fmt.Println(d.display())
 
 }
